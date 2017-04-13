912edcf9a93b02a8ad7892c04c0d7e5f6f8b95f0
James Sullivan <jamesfsullivan5@gmail.com>
2017-04-12 18:56:38 -0600

    Add final refactor.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 onPageLoad()
 
+function onPageLoad() {
+  appendFromArray(getTenFromStorage(getCardsOnLoad()));
+}
+
 function appendFromArray(getStorage) {
   getStorage.forEach(function (element) {
-    prependCard(element)
+    prependCard(element);
   })
 }
 
@@ -11,74 +15,31 @@ function getTenFromStorage(getStorage) {
   return arrayTen
 }
 
-function hideCompletedToDos() {
-  $('.toggle-card-disabled').hide()
+function getCardsOnLoad() {
+  var cards = getStorage();
+  cards = cards.filter(function(element) {
+    return element.completed.indexOf('show') != -1;
+  })
+  return cards
 }
 
-function showCompletedToDos() {
-  $('.toggle-card-disabled').show()
-}
-
-function onPageLoad() {
-  appendFromArray(getTenFromStorage(getCardsOnLoad()))
-  // hideCompletedToDos()
-}
-
-
-function disableShowMore() {
-  $('.show-more-btn').hide()
-}
-
-$('.show-more-btn').on('click', function() {
-  clearCards();
-  appendFromArray(getStorage());
-  disableShowMore();
-  showCompletedToDos();
-
-})
-
-$('header').on('input', '.body-storage, .title-storage', enableSave)
-$('.save-btn').on('click', saveCard)
-$('.save-btn').on('click', clearInputFields)
-$('.save-btn').on('click', enableSave)
-function saveCard() {
-storeLocally(updateCardArray(getStorage(), addCard()));
-prependCard(addCard());
-}
-function Idea(title, body) {
+function ToDo(title, body) {
   this.title = title;
   this.body = body;
   this.id = Date.now();
-  this.importanceArray = ['None', 'Low', 'Normal', 'High', 'Critical']
-  this.importanceLevel = 0
+  this.importanceArray = ['None', 'Low', 'Normal', 'High', 'Critical'];
+  this.importanceLevel = 0;
   this.importance = this.importanceArray[this.importanceLevel];
   this.completed = 'show';
 }
 
-$('.card-container').on('click', '.toggle-complete', updateCompletedLocalStorage)
-
-function updateCompletedLocalStorage() {
-  var cardId = $(this).closest('.todo-card').attr('id');
-  var tempArray = getStorage();
-  $(this).closest('.todo-card').addClass('toggle-card-disabled');
-  updateCompleted(cardId, tempArray);
-  storeLocally(tempArray);
-}
-
-function updateCompleted(cardId, newArray) {
-  newArray.forEach(function(element, index, array) {
-    if (element.id == cardId) {
-      element.completed = 'toggle-card-disabled';
-    }
-  })
-}
-
 addCard = function() {
-var $title = $('.title-storage').val();
-var $body = $('.body-storage').val()
-var idea = new Idea($title, $body)
-return idea
+  var $title = $('.title-storage').val();
+  var $body = $('.body-storage').val();
+  var todo = new ToDo($title, $body);
+  return todo
 }
+
 function prependCard(addCard) {
   $('.card-container').prepend(
     `<article class="todo-card ${addCard.completed}" id=${addCard.id}>
@@ -97,24 +58,58 @@ function prependCard(addCard) {
     </article>`
   );
 }
-function getStorage() {
-  var getItem =  localStorage.getItem('key') || '[]'
-  getItem = JSON.parse(getItem)
-  return getItem
-}
-function updateCardArray(getStorage, addCard) {
-  var getStorage1 = getStorage
-  getStorage1.push(addCard)
-  return getStorage1
-}
+
 function storeLocally(updateCardArray) {
   var stringifiedArray = JSON.stringify(updateCardArray);
   localStorage.setItem('key', stringifiedArray);
 }
+
+function getStorage() {
+  var getItem =  localStorage.getItem('key') || '[]';
+  getItem = JSON.parse(getItem);
+  return getItem
+}
+
+function updateCompletedLocalStorage() {
+  var cardId = $(this).closest('.todo-card').attr('id');
+  var tempArray = getStorage();
+  $(this).closest('.todo-card').addClass('toggle-card-disabled');
+  updateCompleted(cardId, tempArray);
+  storeLocally(tempArray);
+}
+
+function updateCompleted(cardId, newArray) {
+  newArray.forEach(function(element, index, array) {
+    if (element.id == cardId) {
+      element.completed = 'toggle-card-disabled';
+    }
+  })
+}
+
+function updateCardArray(getStorage, addCard) {
+  var getStorage1 = getStorage;
+  getStorage1.push(addCard);
+  return getStorage1
+}
+
+function showCompletedToDos() {
+  $('.toggle-card-disabled').show();
+}
+
+function disableShowMore() {
+  $('.show-more-btn').hide();
+}
+
 function clearInputFields() {
   $('.title-storage').val('');
   $('.body-storage').val('');
 }
+
+function saveCard() {
+storeLocally(updateCardArray(getStorage(), addCard()));
+prependCard(addCard());
+}
+
 function enableSave() {
   var $title = $('.title-storage').val();
   var $body = $('.body-storage').val();
@@ -124,45 +119,50 @@ function enableSave() {
     toggleSaveDisable(true);
   }
 }
+
 function toggleSaveDisable(value) {
   $('.save-btn').prop('disabled', value);
 }
-$('.card-container').on('click', '.upvote-icon', upVote)
-function upVote() {
-  var $cardID = $(this).closest('.todo-card').attr('id')
-  var $upBtn = $(this)
-  storeLocally(increaseImportanceArray(getStorage(), $cardID))
-  clearCards()
-  appendFromArray(getCardsOnLoad())
-}
+
 function increaseImportanceArray(getStorage, $cardID) {
-var tempArray = getStorage
+var tempArray = getStorage;
 for (var i = 0; i < tempArray.length; i++) {
- while ($cardID == tempArray[i].id && tempArray[i].importanceLevel < tempArray[i].importanceArray.length - 1){
-  tempArray[i].importanceLevel++
-  tempArray[i].importance = tempArray[i].importanceArray[tempArray[i].importanceLevel]
+ while ($cardID == tempArray[i].id && tempArray[i].importanceLevel < tempArray[i].importanceArray.length - 1) {
+  tempArray[i].importanceLevel++;
+  tempArray[i].importance = tempArray[i].importanceArray[tempArray[i].importanceLevel];
   break
   }
   continue
 }
 return tempArray
 }
-$('.card-container').on('click', '.downvote-icon', downVote)
+
+function clearCards() {
+  $('.card-container').html('');
+}
+
+function upVote() {
+  var $cardID = $(this).closest('.todo-card').attr('id');
+  var $upBtn = $(this);
+  storeLocally(increaseImportanceArray(getStorage(), $cardID));
+  clearCards();
+  appendFromArray(getCardsOnLoad());
+}
 
 function downVote() {
-  var $cardID = $(this).closest('.todo-card').attr('id')
-  var $upBtn = $(this)
-  storeLocally(decreaseImportanceArray(getStorage(), $cardID))
-  clearCards()
-  appendFromArray(getCardsOnLoad())
+  var $cardID = $(this).closest('.todo-card').attr('id');
+  var $upBtn = $(this);
+  storeLocally(decreaseImportanceArray(getStorage(), $cardID));
+  clearCards();
+  appendFromArray(getCardsOnLoad());
 }
 
 function decreaseImportanceArray(getStorage, $cardID) {
-var tempArray = getStorage
+var tempArray = getStorage;
   for (var i = 0; i < tempArray.length; i++) {
     while ($cardID == tempArray[i].id && tempArray[i].importanceLevel > 0){
-      tempArray[i].importanceLevel--
-      tempArray[i].importance = tempArray[i].importanceArray[tempArray[i].importanceLevel]
+      tempArray[i].importanceLevel--;
+      tempArray[i].importance = tempArray[i].importanceArray[tempArray[i].importanceLevel];
       break
       }
     continue
@@ -170,60 +170,31 @@ var tempArray = getStorage
   return tempArray
 }
 
-function clearCards() {
-  $('.card-container').html('')
+function deleteCard() {
+var $cardID = $(this).closest('.todo-card').attr('id')
+var $card = $(this).closest('.todo-card')
+storeLocally(deleteObject(getStorage(), $cardID, $card))
 }
 
 function deleteObject(getStorage, $cardID, $card) {
-  var newArray = getStorage
+  var newArray = getStorage;
   newArray.forEach(function(element, index, array) {
     if (element.id == $cardID) {
-      console.log(element)
-      console.log(element.id, "made it past")
-      console.log(array)
-      newArray.splice(index, 1)
-    }
-    else {
+      newArray.splice(index, 1);
+    } else {
       return newArray
     }
   })
-  $($card).remove()
-  return newArray;
+  $($card).remove();
+  return newArray
 }
-
-$('.card-container').on('click', '.delete-icon', deleteCard)
-  function deleteCard() {
-  var $cardID = $(this).closest('.todo-card').attr('id')
-  var $card = $(this).closest('.todo-card')
-  storeLocally(deleteObject(getStorage(), $cardID, $card))
-}
-
-$('.search-input').on('input', searchCards)
-
-function searchCards() {
-  var something = $(this)
-  var searchInput = $('.search-input').val().toLowerCase();
-  $('.searchable').each(function() {
-    var cardText = $(this).text().toLowerCase();
-    if (cardText.indexOf(searchInput) != -1) {
-      $(this).parent().show();
-    } else {
-      $(this).parent().hide();
-    }
-  })
-}
-
-$('.card-container').on('keyup', 'h2', saveEditsTitle)
-$('.card-container').on('keyup', 'p', saveEditsBody)
-
 
 function saveEditsTitle(event) {
-    console.log(this);
     var toDoText = $(this).text();
     var $cardId = $(this).closest('.todo-card').attr('id');
     var newArray = getStorage();
-    enterKeyTitle(toDoText, $cardId, newArray)
-    storeLocally(newArray)
+    enterKeyTitle(toDoText, $cardId, newArray);
+    storeLocally(newArray);
 }
 
 function enterKeyTitle(toDoText, $cardId, newArray) {
@@ -237,25 +208,22 @@ function updateTitleText(toDoText, cardId, newArray) {
   newArray.forEach(function(element, index, array) {
     if (element.id == cardId) {
       element.title = toDoText;
-      console.log(element);
     }
   })
 }
 
 function saveEditsBody(event) {
-    console.log(this);
     var toDoText = $(this).text();
     var $cardId = $(this).closest('.todo-card').attr('id');
     var newArray = getStorage();
-    enterKeyBody(toDoText, $cardId, newArray)
-    storeLocally(newArray)
+    enterKeyBody(toDoText, $cardId, newArray);
+    storeLocally(newArray);
 }
 
 function updateBodyText(toDoText, cardId, newArray) {
   newArray.forEach(function(element, index, array) {
     if (element.id == cardId) {
       element.body = toDoText;
-      console.log(element);
     }
   })
 }
@@ -266,54 +234,88 @@ function enterKeyBody(toDoText, $cardId, newArray) {
     updateBodyText(toDoText, $cardId, newArray);
   }}
 
-var cardsNeg = 1
+function searchCards() {
+  var searchInput = $('.search-input').val().toLowerCase();
+  $('.searchable').each(function() {
+    var cardText = $(this).text().toLowerCase();
+    if (cardText.indexOf(searchInput) != -1) {
+      $(this).parent().show();
+    } else {
+      $(this).parent().hide();
+    }
+  })
+}
 
-  $('main').on('click', '.filter-btn', function() {
-    $(this).toggleClass("filter-btn-clicked", true)
-    if (cardsNeg === 1) {
-      filterCards()
-      }
-      else {
-      filterCards2()
-      }
-      })
+function showMore() {
+  clearCards();
+  appendFromArray(getStorage());
+  disableShowMore();
+  showCompletedToDos();
+}
 
-  function filterCards() {
-    var thisButton = event.target
-    var filterAttribute = $(thisButton).text()
-    var cards = getStorage()
-    cardsNeg = cards.filter(function(element, index) {
-    return element.importance.indexOf(filterAttribute) == -1
-    })
-    cardsNeg.forEach(function (element, index){
-      $('#'+element.id).hide()
-    })}
+$('.show-more-btn').on('click', showMore)
 
-  function filterCards2() {
-    var thisButton = event.target
-    var filterAttribute = $(thisButton).text()
-    var cardsPos = cardsNeg.filter(function(element, index) {
-      return element.importance.indexOf(filterAttribute) != -1
-    })
-    cardsPos.forEach(function (element, index) {
-      $('#'+element.id).show()
-    })
-  }
+$('.card-container').on('click', '.toggle-complete', updateCompletedLocalStorage)
+
+$('header').on('input', '.body-storage, .title-storage', enableSave)
+
+$('.save-btn').on('click', saveCard)
+
+$('.save-btn').on('click', clearInputFields)
+
+$('.save-btn').on('click', enableSave)
+
+$('.card-container').on('click', '.upvote-icon', upVote)
+
+$('.card-container').on('click', '.downvote-icon', downVote)
+
+$('.card-container').on('click', '.delete-icon', deleteCard)
+
+$('.search-input').on('input', searchCards)
 
 $('main').on('click', '.remove-filters-btn', removeFilters)
 
-  function removeFilters() {
-    var cards = getStorage()
-    cards.forEach(function (element, index) {
-      $('#'+element.id).show()
-    })
-    $('.filter-btn').toggleClass('filter-btn-clicked', false)
-  }
+$('.card-container').on('keyup', 'h2', saveEditsTitle)
 
-function getCardsOnLoad() {
+$('.card-container').on('keyup', 'p', saveEditsBody)
+
+var cardsNeg = 1;
+
+$('main').on('click', '.filter-btn', function() {
+  $(this).toggleClass("filter-btn-clicked", true)
+  if (cardsNeg === 1) {
+    filterCards();
+    } else {
+    filterCards2();
+    }
+})
+
+function filterCards() {
+  var thisButton = event.target;
+  var filterAttribute = $(thisButton).text();
   var cards = getStorage();
-  cards = cards.filter(function(element){
-    return element.completed.indexOf('show') != -1
+  cardsNeg = cards.filter(function(element, index) {
+  return element.importance.indexOf(filterAttribute) == -1
   })
-  return cards
+  cardsNeg.forEach(function (element, index){
+    $('#'+element.id).hide();
+  })}
+
+function filterCards2() {
+  var thisButton = event.target;
+  var filterAttribute = $(thisButton).text();
+  var cardsPos = cardsNeg.filter(function(element, index) {
+    return element.importance.indexOf(filterAttribute) != -1
+  })
+  cardsPos.forEach(function (element, index) {
+    $('#'+element.id).show();
+  })
+}
+
+function removeFilters() {
+  var cards = getStorage();
+  cards.forEach(function (element, index) {
+    $('#'+element.id).show();
+  })
+  $('.filter-btn').toggleClass('filter-btn-clicked', false);
 }
